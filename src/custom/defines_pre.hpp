90d7932cf071d7bbcf8864392137b2d4c11044ff
myamasak <yamasaki.marcos@gmail.com>
2022-12-28 14:05:06 -0300

    Remove packet obfuscation; remove renewal casting time; define packetver 20200401

--- src/custom/defines_pre.hpp
+++ src/custom/defines_pre.hpp
@@ -2,6 +2,7 @@
 // For more information, see LICENCE in the main folder
 
 #ifndef CONFIG_CUSTOM_DEFINES_PRE_HPP
+#define PACKETVER 20200401
 #define CONFIG_CUSTOM_DEFINES_PRE_HPP
 
 /**
@@ -9,6 +10,6 @@
  * For detailed guidance on these check http://rathena.org/wiki/SRC/config/
  **/
 
-
+#define PACKETVER 20200401
 
 #endif /* CONFIG_CUSTOM_DEFINES_PRE_HPP */
